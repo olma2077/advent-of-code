7a54b091800c8caee38d8f475cc791b74a6f1a3b
Oleg Matviichuk <ma7hway@gmail.com>
2024-12-10 16:24:27 +0100

    [2015] day 6

---
+++ 2015/06/part_1.py
@@ -0,0 +1,55 @@
+import os
+import re
+import time
+
+INPUT = [
+    'input.tst',
+    'input.txt'
+]
+
+
+def turn_off(x1, y1, x2, y2, lights):
+    for x in range(x1, x2 + 1):
+        for y in range(y1, y2 + 1):
+            lights[y][x] = False
+
+
+def turn_on(x1, y1, x2, y2, lights):
+    for x in range(x1, x2 + 1):
+        for y in range(y1, y2 + 1):
+            lights[y][x] = True
+
+
+def toggle(x1, y1, x2, y2, lights):
+    for x in range(x1, x2 + 1):
+        for y in range(y1, y2 + 1):
+            lights[y][x] = not lights[y][x]
+
+
+def solver(filename):
+    lights = [[False] * 1000 for _ in range(1000)]
+
+    with open(filename) as file:
+        for instruction in file:
+            match re.fullmatch(
+                r"(turn off|toggle|turn on) (\d+),(\d+) through (\d+),(\d+)",
+                instruction.strip()).groups():
+                case ("turn off", x1, y1, x2, y2):
+                    turn_off(*[int(i) for i in (x1, y1, x2, y2)], lights)
+                case ("toggle", x1, y1, x2, y2):
+                    toggle(*[int(i) for i in (x1, y1, x2, y2)], lights)
+                case ("turn on", x1, y1, x2, y2):
+                    turn_on(*[int(i) for i in (x1, y1, x2, y2)], lights)
+                case other:
+                    print(f'Parsing error: {other}')
+
+    return sum(sum(1 for l in row if l) for row in lights)
+
+
+for filename in INPUT:
+    if os.path.exists(filename):
+        start = time.perf_counter()
+        answer = solver(filename)
+        stop = time.perf_counter()
+
+        print(f'Answer for {filename} got in {stop - start:0.6f}s: \n {answer}')
