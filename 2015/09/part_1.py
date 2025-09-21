016347d69ea9bf6d04838a558e021816539e2cbd
Oleg Matviichuk <ma7hway@gmail.com>
2025-09-21 13:13:04 +0200

    [2015] day 9

---
+++ 2015/09/part_1.py
@@ -0,0 +1,49 @@
+import os
+import re
+import time
+
+INPUT = [
+    'input.tst',
+    'input.txt'
+]
+
+
+def shortest_distance(graph, start, path):
+    # backtracking approach with selecting a minimum possible path distance
+    path.append(start)
+    shortest_dst = 0
+
+    # if not all locations visited
+    if len(path) < len(graph):
+        shortest_dst = float("inf")
+
+        for next_loc, dst in graph[start]:
+            if next_loc not in path:
+                shortest_dst = min(
+                    shortest_dst,
+                    dst + shortest_distance(graph, next_loc, path)
+                )
+
+    path.pop()
+    return shortest_dst
+
+
+def solver(filename):
+    graph = {}
+
+    with open(filename) as file:
+        for edge in file:
+            src, dst, weight = re.fullmatch(r"(\w+) to (\w+) = (\d+)", edge.strip()).groups()
+            graph[src] = graph.get(src, []) + [(dst, int(weight))]
+            graph[dst] = graph.get(dst, []) + [(src, int(weight))]
+
+    return min([shortest_distance(graph, start, []) for start in graph])
+
+
+for filename in INPUT:
+    if os.path.exists(filename):
+        start = time.perf_counter()
+        answer = solver(filename)
+        stop = time.perf_counter()
+
+        print(f'Answer for {filename} got in {stop - start:0.6f}s: \n {answer}')
