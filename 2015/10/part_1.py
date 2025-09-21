4826ed2b3c52bc106a95f64e6eed1be24f0b1a9b
Oleg Matviichuk <ma7hway@gmail.com>
2025-09-21 13:42:49 +0200

    [2015] day 10

---
+++ 2015/10/part_1.py
@@ -0,0 +1,43 @@
+import os
+import time
+
+INPUT = [
+    'input.tst',
+    'input.txt'
+]
+
+
+def look_and_say(number):
+    prev, count = number[0], 0
+    new_number = ''
+
+    for c in number:
+        if c == prev:
+            count += 1
+        else:
+             new_number += str(count) + prev
+             prev = c
+             count = 1
+
+    new_number += str(count) + prev
+
+    return new_number
+
+def solver(filename):
+    number = ''
+
+    with open(filename) as file:
+        number = file.readline()
+
+    for _ in range(40):
+        number = look_and_say(number)
+
+    return len(number)
+
+for filename in INPUT:
+    if os.path.exists(filename):
+        start = time.perf_counter()
+        answer = solver(filename)
+        stop = time.perf_counter()
+
+        print(f'Answer for {filename} got in {stop - start:0.6f}s: \n {answer}')
